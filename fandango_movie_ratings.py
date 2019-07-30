93195fd49a507873d86015b78f805a6ee9fbc98a
bbchen33 <52116103+bbchen33@users.noreply.github.com>
2019-07-29 21:04:13 -0600

    Create fandango_movie_ratings.py
    Exercise provided by https://www.dataquest.io/blog/data-science-project-fandango/
    I expanded the exercise by including joining data frames, a permutation test and data visualization.

---
+++ fandango_movie_ratings.py
@@ -0,0 +1,67 @@
+import pandas as pd
+fandango_score = pd.read_csv('fandango_score_comparison.csv')
+movie_ratings = pd.read_csv('movie_ratings_16_17.csv')
+print(fandango_score.head(5))
+print(movie_ratings.head(5))
+# use .shape to learn the shape of data
+fandango_score.shape
+new_columns = []
+# for loop to capture all the columns with fandango in the name
+for subject in fandango_score.columns:
+	if 'Fandango' in subject:
+		new_columns.append(subject)
+# add FILM to the first column
+new_columns.insert(0,'FILM')
+fandango_previous = fandango_score[new_columns].copy()
+
+fandango_after = movie_ratings[['movie','year','fandango']].copy()
+
+import matplotlib.pyplot as plt
+fan_reviews = plt.hist(fandango_previous['Fandango_votes'], range=[0,100], bins = 20)
+# set the x range between 0 and 100 only since we only care if there're reviews < 30
+plt.axvline(30, color = 'r', linestyle = 'dashed')
+# we also want to see how the data distributed around our cutoff line x = 30
+plt.show()
+# confirm that all movies here have votes 30 or more votes
+under_30 = sum(fandango_previous['Fandango_votes'] < 30)
+# add the column year by extracting year info from FILM
+fandango_previous['year'] = fandango_previous['FILM'].str[-5:-1]
+# make a new table with only movies from 2015 and 2016. Note 2016 in fandango_after is an integer. 
+fandango_2015 = fandango_previous[fandango_previous['year']=='2015'].copy()
+fandango_2016 = fandango_after[fandango_after['year'] == 2016].copy()
+fandango_2015.shape
+fandango_2016.shape
+# 129 movies in 2015 and 191 movies in 2016
+# changing the year data in 2015 table from str to integer so I can join the 2015 and 2016 data frames
+import numpy as np
+fandango_2015.year = pd.to_numeric(fandango_2015.year, errors = 'coerce').astype(np.int64)
+
+fandango_2016.columns = ['FILM','year','Fandango_Stars']
+# use concat function to merge 2 dataframes
+fandango_15_and_16 = pd.concat([fandango_2015,fandango_2016], join = 'outer',sort=True)
+
+#actual average ratings
+total_average = fandango_15_and_16.Fandango_Stars.mean()
+## Conduct a permutation test to determine if there is a difference in the average ratings from 2015 and 2016
+#randomly select 129 samples and assign them as ratings from 2015
+fifteen_avg_list = []
+sixteen_avg_list = []
+for i in range(100):
+	random15 = fandango_15_and_16.sample(129)
+	fifteen_avg_list.append(random15.Fandango_Stars.mean())
+	sixteen_avg_list.append((fandango_15_and_16.Fandango_Stars.sum()-random15.Fandango_Stars.sum())/191)
+
+# calculate all the mean differences from the for loop
+difference = np.array(fifteen_avg_list) - np.array(sixteen_avg_list)
+actual_difference = fandango_2015.Fandango_Stars.mean() - fandango_2016.Fandango_Stars.mean()
+
+Extreme_chances = np.sum(difference>abs(actual_difference))+np.sum(difference<-abs(actual_difference))
+# p value
+probability = Extreme_chances/200
+# As the p value is < 0.05, it means the probability that given what we observed, the chance that there is no difference is extremely unlikely --> there is a difference
+# Examine the distribution of the mean differences obtained from permutation test
+plt.hist(difference, bins = 30)
+# add two dash lines to show where the actual difference lies in all the test samples obtained
+plt.axvline(actual_difference, color = 'r', linestyle = 'dashed')
+plt.axvline(-actual_difference, color = 'r', linestyle = 'dashed')
+plt.show()
